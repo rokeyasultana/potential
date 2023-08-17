663d90b868ffe79b8a60b36128c8d8a11629fca4
rokeyasultana <rokeyasbd@gmail.com>
2023-08-17 21:43:27 +0600

    Review

--- index.js
+++ index.js
@@ -34,3 +34,44 @@ document.addEventListener("DOMContentLoaded", function() {
 
   showSlide(currentSlide);
 });
+
+// 
+const slides = document.querySelectorAll('.review-slide');
+const arrowLeft = document.querySelector('.arrow-left');
+const arrowRight = document.querySelector('.arrow-right');
+let currentIndex = 0;
+
+function showSlide(index) {
+  slides.forEach(slide => {
+    slide.style.display = 'none';
+  });
+
+  slides[index].style.display = 'block';
+}
+
+function changeSlide(offset) {
+  slides[currentIndex].style.display = 'none';
+  currentIndex = (currentIndex + offset + slides.length) % slides.length;
+  slides[currentIndex].style.display = 'block';
+}
+
+arrowLeft.addEventListener('click', () => {
+  changeSlide(-1);
+});
+
+arrowRight.addEventListener('click', () => {
+  changeSlide(1);
+});
+
+document.addEventListener('keydown', event => {
+  if (event.key === 'ArrowLeft') {
+    changeSlide(-1);
+  } else if (event.key === 'ArrowRight') {
+    changeSlide(1);
+  }
+});
+
+// Show the first slide initially
+showSlide(currentIndex);
+
+
